c385442fe75a45e9621645ed11928b4451876f24
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-24 20:39:25 +0545

    Updated models.py

--- to_do_app/todo_task/models.py
+++ to_do_app/todo_task/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    event_name = models.CharField(max_length = 250)
+    event_detail = models.TextField(max_length = 1000)
+    created_by = models.CharField(max_length = 100)
+    event_time = models.TimeField(auto_now_add = True)
+    event_date = models.DateField(auto_now_add = True)

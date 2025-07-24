590b7af080feee02c56769270c866ceeacc19a41
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-24 08:43:32 +0545

    Updated layout.html

--- to_do_app/users/views.py
+++ to_do_app/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_register(request):
+    pass
+
+def user_login(request):
+    pass
+
+def user_logout(request):
+    pass

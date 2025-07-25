47c1c28031f0eda37c497db16112cb308e25448f
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-25 15:44:25 +0545

    Updated urls.py

--- to_do_app/todo_task/urls.py
+++ to_do_app/todo_task/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'todo_task'
 
 urlpatterns = [
-    path('lists/', views.task_list, name = 'task-list')
+    path('lists/', views.task_list, name = 'task-list'),
+    path('details/<int:id>', views.task_detail, name = 'task-detail' )
     
 ]

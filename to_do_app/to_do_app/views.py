06451f14c34e1994a3a7ff3205ad30a2b60af8d5
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-27 08:24:45 +0545

    Updated views.py

--- to_do_app/to_do_app/views.py
+++ to_do_app/to_do_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 
 def home(request):
     return render(request, 'homepage.html')
 
-
+@login_required(login_url = "users:user-login")
 def about(request):
     return render(request, 'aboutpage.html')

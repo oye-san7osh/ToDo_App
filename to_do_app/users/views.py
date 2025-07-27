10e628cffd6820b45dbaa7f449150c921d89380d
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-27 07:02:05 +0545

    Updated views.py

--- to_do_app/users/views.py
+++ to_do_app/users/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from users.forms import CustomUserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def user_register(request):
-    return render(request, "users/register.html")
+    if request.method == "POST":
+        form = CustomUserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect("users:user-login")
+    else:
+        form = CustomUserRegisterForm()
+    return render(request, "users/register.html", {"form" : form})
+            
 
 def user_login(request):
     return render(request, "users/login.html")

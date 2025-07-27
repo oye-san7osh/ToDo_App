500955be1dccb342d59aa0e44bf98b9f4915107a
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-27 08:25:21 +0545

    Updated views.py

--- to_do_app/todo_task/views.py
+++ to_do_app/todo_task/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from . models import Task
 
 
 # Create your views here.
-
+@login_required(login_url = "users:user-login")
 def task_list(request):
     tasklists = Task.objects.all()
     return render(request, "todo_task/task_list.html",
                   {"tasklists": tasklists})
     
-    
+@login_required(login_url = "users:user-login")
 def task_detail(request, slug_link):
     taskdetail = Task.objects.get(slug = slug_link)
     return render(request, "todo_task/task_details.html",

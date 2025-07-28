51aa274325af8a7a841965264bf3a144d0917bfc
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-28 19:08:00 +0545

    Updated views.py

--- to_do_app/todo_task/views.py
+++ to_do_app/todo_task/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from . models import Task
+from . import forms
 
 
 # Create your views here.
-@login_required(login_url = "users:user-login")
+
 def task_list(request):
-    tasklists = Task.objects.all()
+    if request.user.is_superuser:
+        tasklists = Task.objects.all().order_by('-event_date')
+    else:
+        tasklists = Task.objects.filter(created_by = request.user).order_by('-event_date')
     return render(request, "todo_task/task_list.html",
                   {"tasklists": tasklists})
     
-@login_required(login_url = "users:user-login")
+
 def task_detail(request, slug_link):
-    taskdetail = Task.objects.get(slug = slug_link)
+    taskdetail = Task.objects.get(slug_link = slug_link)
     return render(request, "todo_task/task_details.html",
                   {"taskdetail": taskdetail})
+    
+   
+def new_task(request):
+    if request.method == "POST":
+        form = forms.CreateTask(request.POST, request.FILES)
+        if form.is_valid():
+            task = form.save(commit = False)
+            task.created_by = request.user
+            task.save()
+            return redirect("todo_task:task-list")
+    else:
+        form = forms.CreateTask()
+    return render(request, "todo_task/task_new.html", {"form": form} )

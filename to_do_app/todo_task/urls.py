3ebb2951b88ff7358494510afccfc0870013fb3d
Santosh Paudel <santoshpaudel322@gmail.com>
2025-07-30 15:19:07 +0545

    Updated urls.py

--- to_do_app/todo_task/urls.py
+++ to_do_app/todo_task/urls.py
@@ -6,6 +6,7 @@ app_name = 'todo_task'
 urlpatterns = [
     path('lists/', views.task_list, name = 'task-list'),
     path('details/<slug:slug_link>', views.task_detail, name = 'task-detail' ),
-    path('new_task/', views.new_task, name = 'new-task')
-    
+    path('new_task/', views.new_task, name = 'new-task'),
+    path('update/<slug:slug_link>', views.update_task, name = 'update-task'),
+    path('delete/<slug:slug_link>', views.delete_task, name = 'delete-task'),
 ]
